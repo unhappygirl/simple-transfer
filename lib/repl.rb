700c9ec90ba0717ffd2bca8cac434039f36f85f9
teknosa <aydin.turanli@sabanciuniv.edu>
2025-02-23 10:54:20 +0300

    initial

---
+++ lib/repl.rb
@@ -0,0 +1,85 @@
+
+require_relative "transfer.rb"
+
+class STShell
+    def initialize(server_addr, port)
+        @port = port
+        @client = SimpleTransfer::Sender.new(server_addr, port)
+    end
+
+    def self.list_files(path)
+        Dir.entries(path).each do |f|
+            if File.directory?(f)
+                puts "#{f}/"
+            else
+                puts f
+            end
+        end
+    end
+
+    
+    def eval(query)
+        p1, p2 = query.split(" ")
+        if p1 == "clear"
+            system "clear"
+        elsif p1 == "send_file"
+            @client.send_file(p2)
+        elsif p1 == "send_directory"
+            @client.send_directory(p2)
+        elsif p1 == "cd"
+            Dir.chdir(p2)
+        elsif p1 == "ls"
+            if p2.nil?
+              STShell.list_files(Dir.pwd)
+            else
+                STShell.list_files(p2)
+            end
+        elsif p1 == "help"
+            help()
+        else
+            p "I don't recognize this command: `#{p1}` friend!"
+        end
+    end
+
+    def help()
+        puts "Type 'send_file <filename>' to send a file"
+        puts "Type 'send_directory <directory>' to send a directory"
+        puts "Type 'cd <directory>' to change directory"
+        puts "Type 'ls <directory>' to list directory contents"
+        puts "Type help to see this message"
+    end
+
+    def read()
+        query = $stdin.gets.chomp
+        #puts query, echo is very annoying
+        return query
+    end
+
+    def print()
+        Kernel.print("[ST]🍀 #{Dir.pwd} >>>> ")
+    end
+
+    def init_loop()
+        puts "STShell initialized on port #{@port}!"
+        puts "Hello friend, thank you for using simple transfer!"
+        puts "Are you ready to drown in simplicity?"
+        puts "It is indeed extremely simple:"
+        help()
+        repl()
+        #real...
+        # let it be
+    end
+
+    def repl()
+        while true
+            print()
+            query = read()
+            begin
+                eval(query)
+            rescue => e
+                p e
+                puts "An error occurred, friend! >:3"
+            end
+        end
+    end
+end
---
+++ simple-transfer.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+    s.name      = 'simple-transfer'
+    s.version   = '1.0.0'
+    s.platform  = Gem::Platform::RUBY
+    s.summary   = 'A gem for transfering files over your network with extreme simplicity -and fast.'
+    s.description = "An extremely simple gem that relies on the TCP stack for transfer of files over a IP network.
+    It is as simple as it gets. Relies on no other protocol."
+    s.authors   = ['Burzum']
+    s.email     = ['atifaydinturanli@gmail.com']
+    s.homepage  = 'http://rubygems.org/gems/pagekey'
+    s.license   = 'MIT'
+    s.files     = Dir.glob("{lib,bin}/**/*")
+    s.require_path = 'lib'
+    s.bindir = 'bin'
+    s.executables = ['simple-transfer']
+  end
